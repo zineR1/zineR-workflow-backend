b4e3a43d5734951bb416fd953f22e8827cd44d3c
zineR1 <agusdalvit@gmail.com>
2026-04-01 03:28:27 -0300

    fix(conect backend front)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { AgentsModule } from './agents/agents.module';
 import { ExecutionsModule } from './executions/executions.module';
 import { LlmModule } from './llm/llm.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [AgentsModule, ExecutionsModule, LlmModule],
+  imports: [AgentsModule, ExecutionsModule, LlmModule, ProjectsModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
